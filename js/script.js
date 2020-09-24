47ef3a308b10814732d4364f4e62020fd3a1a7d9
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-09-24 00:30:55 -0700

    addVideoStream

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@
  let handleFail = function(err){
     console.log("Error : ", err);
  };
+
+ // Queries the container in which the remote feeds belong
+ let remoteContainer = document.getElementById("remote-container");
+/**
+ * @name addVideoStream
+ * @param elementId is the name of the element that is to be inserted into the DOM
+ * @description Helper function to add the video stream to "remote-container"
+ */
+function addVideoStream(elementId){
+    let streamDiv=document.createElement("div"); //create new div for every
+}
