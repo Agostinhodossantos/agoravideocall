dfec30e107b81e02817f287dae5b8dfc50b73aac
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-09-24 01:22:51 -0700

    client methods

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
  * @description Helper function to handle errors
  */
 
- let handleFail = function(err){
+let handleFail = function(err){
     console.log("Error : ", err);
  };
 
@@ -31,4 +31,50 @@ function removeVideoStream(elementId){
     let remDiv = document.getElementById(elementId);
     if(remDiv) remDiv.parentNode.removeChild(remDiv);
 }
+
+let client = AgoraRTC.createElement({
+    node: 'rtc',
+    codec = 'vp8',
+});
+
+client.init('appIdHere');
+
+client.join(null ,'any-channel' ,  null, (uid)=>{
+    let localStream = AgoraRTC.createStream({
+        video: true,
+        audio: true
+    });
+
+    localStream.init(()=>{
+        localStream.play('me');
+        client.publish(localStream , handleFail);
+    }, handleFail);
+} , handleFail);
+
+client.on('stream-added' , function(evt){
+    client.subcribe(evt.stream , handleFail);
+});
+
+client.on('stream-subscribed' , function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
+});
+
+client.on('stream-removed' ,function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close;
+    removeVideoStream(streamId);
+} );
+
+
+client.on('peer-leave' , function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close;
+    removeVideoStream(streamId);
+} );
+
 
