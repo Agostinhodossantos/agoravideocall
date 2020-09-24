4689fbfaa07a8773c3cdf792ba7c88618b51ef2d
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-09-24 00:24:41 -0700

    handleFail

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**
+ * @name handleFail
+ * @param err - error thrown by any function
+ * @description Helper function to handle errors
+ */
+
+ let handleFail = function(err){
+    console.log("Error : ", err);
+ };
